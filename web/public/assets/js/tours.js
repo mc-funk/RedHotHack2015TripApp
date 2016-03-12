f3dd21aa155475d730d3882de31f8201d0ce67ce
Michelle C. Funk <michelle@michellefunk.com>
2016-03-12 16:37:10 -0600

    Angular Integration for object (database sub)

--- web/public/assets/js/tours.js
+++ web/public/assets/js/tours.js
@@ -1,51 +1,59 @@
 var app=angular.module('app', []);
 
 console.log("tours.js loading");
-app.controller("DeviceController", ['$scope', '$http', function($scope, $http) {
-  $scope.tour = {};
+app.controller("ToursController", ['$scope', '$http', function($scope, $http) {
   $scope.allTours = [];
+  $scope.tour = {};
+  $scope.stop = {};
+  $scope.tag = "";
   console.log("app.controller entered");
 
   function getTours () {
-    $scope.allTours = {
-      RedWingTour: [
+    $scope.allTours = [
         {
-          name: "Alexander P. Anderson Estate-Tower View",
-          latlong: "44.569922, -92.638006",
-          detail: "The former Tower View Farm was listed on the National Register of Historic Places on April 13, 1977. The property is now the location of the Anderson Center, site of a 700 residency program for artists, writers, and scholars. The A. P. Anderson award is presented annually by the Anderson Center to recognize significant contributions to the cultural and artistic life of Minnesota.",
-          category: "historical bulidings",
-          images: []
-        },
-        {
-          name: "Chicago Great Western Depot",
-          latlong:"46.729553; -94.6859",
-          detail:"The Chicago Great Western Depot at 726 East Main Street in Red Wing, in the U.S. State of Minnesota, is a former railroad station listed on the National Register of Historic Places. The depot was built in 1906 after the Chicago Great Western Railway (CGW) acquired two small railroads: the Minnesota Central, which connected to Cannon Falls; and the Duluth, Red Wing, and Southern, which connected to Zumbrota. CGW established a divisional headquarters and built this depot in 1906, with passenger space on the first floor and office space on the second floor. The red brick structure was the largest depot the railroad built in Minnesota."
-          category: "historical bulidings"
-        },
-        {
-          name: "James L. Lawther House",
-          latlong: "46.729553;-94.6859",
-          detail: "the James L. Lawther House is an octagon house in Red Wing, Minnesota, United States. The owner, James Lawther, built it after he visited Dubuque, Iowa and toured the Langworthy House there. At the time, the Langworthy House was the grandest house of its type on the upper Mississippi River. The Lawther House was built in 1857, with an addition in 1870.[2] Its location, at the corner of Third and Hill Streets, has been termed "one of the most architecturally significant intersections in Minnesota" and is part of the Red Wing Residential Historic District. It is listed on the National Register of Historic Places."
-          category: "historical bulidings",
-          images: []
-        },
-        {
-          name: "Red Wing Iron Works",
-          latlong: "44.566069; -92.536956",
-          detail: "Red Wing's oldest surviving industrial building, an 1874 ironworks whose supply and repair of local machinery was key to the city's rise as an early manufacturing center."
-          category: "historical bulidings",
-          images: []
-        },
-        {
-          name: "Theodore B. Sheldon House",
-          latlong: "44.561765, -92.538158",
-          detail: "1875 house of Theodore B. Sheldon (1820–1900), an early settler of Red Wing who arrived in 1856 and became one of the city's leading citizens, active in commerce, transportation, and civics.[66] Also a contributing property to the Red Wing Residential Historic District.",
-          category: "historical bulidings",
-          images: []
-        }
-      ]
+          name: "Red Wing Historic Gems",
+          creator: "Red Wing Historical Society",
+          creatorInfo: "The Red Wing Historical Society is dedicated to increasing community engagement with Red Wing's historic treasures.",
+          tags: ["historic", "depots", "houses", "factories"],
+          stops: [{
+            name: "Alexander P. Anderson Estate-Tower View",
+            latlong: "44.569922, -92.638006",
+            detail: "The former Tower View Farm was listed on the National Register of Historic Places on April 13, 1977. The property is now the location of the Anderson Center, site of a 700 residency program for artists, writers, and scholars. The A. P. Anderson award is presented annually by the Anderson Center to recognize significant contributions to the cultural and artistic life of Minnesota.",
+            category: "historical bulidings",
+            images: []
+            },
+            {
+              name: "Chicago Great Western Depot",
+              latlong:"46.729553; -94.6859",
+              detail:"The Chicago Great Western Depot at 726 East Main Street in Red Wing, in the U.S. State of Minnesota, is a former railroad station listed on the National Register of Historic Places. The depot was built in 1906 after the Chicago Great Western Railway (CGW) acquired two small railroads: the Minnesota Central, which connected to Cannon Falls; and the Duluth, Red Wing, and Southern, which connected to Zumbrota. CGW established a divisional headquarters and built this depot in 1906, with passenger space on the first floor and office space on the second floor. The red brick structure was the largest depot the railroad built in Minnesota.",
+              category: "historical bulidings"
+            },
+            {
+              name: "James L. Lawther House",
+              latlong: "46.729553;-94.6859",
+              detail: "the James L. Lawther House is an octagon house in Red Wing, Minnesota, United States. The owner, James Lawther, built it after he visited Dubuque, Iowa and toured the Langworthy House there. At the time, the Langworthy House was the grandest house of its type on the upper Mississippi River. The Lawther House was built in 1857, with an addition in 1870.[2] Its location, at the corner of Third and Hill Streets, has been termed 'one of the most architecturally significant intersections in Minnesota' and is part of the Red Wing Residential Historic District. It is listed on the National Register of Historic Places.",
+              category: "historical bulidings",
+              images: []
+            },
+            {
+              name: "Red Wing Iron Works",
+              latlong: "44.566069; -92.536956",
+              detail: "Red Wing's oldest surviving industrial building, an 1874 ironworks whose supply and repair of local machinery was key to the city's rise as an early manufacturing center.",
+              category: "historical bulidings",
+              images: []
+            },
+            {
+              name: "Theodore B. Sheldon House",
+              latlong: "44.561765, -92.538158",
+              detail: "1875 house of Theodore B. Sheldon (1820–1900), an early settler of Red Wing who arrived in 1856 and became one of the city's leading citizens, active in commerce, transportation, and civics.[66] Also a contributing property to the Red Wing Residential Historic District.",
+              category: "historical bulidings",
+              images: []
+            }]
+          }
+        ];
+
     }
-    console.log("allTours: ", $scope.allTours);
-    return $scope.allTours();
-  }
+    getTours();
+    console.log("$scope.allTours: ", $scope.allTours);
+    return $scope.allTours;
 }]);
